2982b37f163ebd50b3759349a2e21d6b893369cb
Balazs Laczko <laczbali@gmail.com>
2022-09-05 18:10:27 +0200

    added dotnet grpc client

---
+++ grpc/grpc_client/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+
+namespace grpc_client
+{
+	public class Program
+	{
+		static async Task Main(string[] args)
+		{
+			using var channel = GrpcChannel.ForAddress("http://localhost:5024");
+			var client = new Greeter.GreeterClient(channel);
+
+			var reply = await client.SayHelloAsync(new HelloRequest { Name = "Balazs"});
+			Console.WriteLine($"Server says: [ {reply.Message} ]");
+		}	
+	}
+}

b57f13ca2e81ef00abffac35ed08a7ef3d66513d
Balazs Laczko <laczbali@gmail.com>
2022-09-05 16:56:02 +0200

    added bi-directional streaming to backend

--- grpc/grpc_backend/Services/GreeterService.cs
+++ grpc/grpc_backend/Services/GreeterService.cs
@@ -1,12 +1,13 @@
 using Grpc.Core;
 using grpc_backend;
+using System.Diagnostics;
 
 namespace grpc_backend.Services
 {
 	public class GreeterService : Greeter.GreeterBase
 	{
 		private readonly ILogger<GreeterService> _logger;
-		public GreeterService(ILogger<GreeterService> logger) 
+		public GreeterService(ILogger<GreeterService> logger)
 		{
 			_logger = logger;
 		}
@@ -33,5 +34,46 @@ namespace grpc_backend.Services
 				await Task.Delay(1000);
 			}
 		}
+
+		public override async Task<TimeStampCollection> ClientTime(IAsyncStreamReader<TimestampReply> requestStream, ServerCallContext context)
+		{
+			var response = new TimeStampCollection();
+
+			await foreach (var request in requestStream.ReadAllAsync())
+			{
+				response.Timestamps.Add(new TimestampReply { Timestamp = request.Timestamp });
+			}
+
+			return response;
+		}
+
+		public override async Task TimeExchange(IAsyncStreamReader<TimestampReply> requestStream, IServerStreamWriter<TimestampReply> responseStream, ServerCallContext context)
+		{
+			var sendTask = SendTime(responseStream, context);
+			var receiveTask = ReceiveTime(requestStream, context);
+
+			await Task.WhenAll(sendTask, receiveTask);
+		}
+
+		private async Task SendTime(IServerStreamWriter<TimestampReply> responseStream, ServerCallContext contex)
+		{
+			while(!contex.CancellationToken.IsCancellationRequested)
+			{
+				await responseStream.WriteAsync(new TimestampReply
+				{
+					Timestamp = ((DateTimeOffset)DateTime.Now).ToUnixTimeSeconds()
+				});
+				await Task.Delay(3000);
+			}
+		}
+
+		private async Task ReceiveTime(IAsyncStreamReader<TimestampReply> requestStream, ServerCallContext context)
+		{
+			while(await requestStream.MoveNext() && !context.CancellationToken.IsCancellationRequested)
+			{
+				Debug.WriteLine(requestStream.Current.Timestamp);
+			}
+		}
+
 	}
 }
